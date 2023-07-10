6fd88fcc300f12d14b6a072d845f6c05cbd7d262
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-07-10 16:10:07 +0900

    02_02ここだけ変更

--- MatrixCalculation.cpp
+++ MatrixCalculation.cpp
@@ -78,25 +78,19 @@ Matrix4x4 MakeRotateZmatrix(float radian) {
 //平行移動
 Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
 	Matrix4x4 result;
-	result.m[0][0] = 1.0f;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
+	for (int i = 0; i < 4; i++) {
+		for (int y = 0; y < 4; y++) {
+			result.m[i][y] = 0;
+		}
+	}
 
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = 1.0f;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = 1.0f;
-	result.m[2][3] = 0.0f;
+	for (int i = 0; i < 4; i++) {
+		result.m[i][i] = 1;
+	}
 
 	result.m[3][0] = translate.x;
 	result.m[3][1] = translate.y;
 	result.m[3][2] = translate.z;
-	result.m[3][3] = 1.0f;
 
 	return result;
 };
@@ -105,25 +99,16 @@ Matrix4x4 MakeTranslateMatrix(Vector3 translate) {
 Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 	Matrix4x4 result;
 
+	for (int i = 0; i < 4; i++) {
+		for (int y = 0; y < 4; y++) {
+			result.m[i][y] = 0;
+		}
+	}
+
 	result.m[0][0] = scale.x;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = 0.0f;
 	result.m[1][1] = scale.y;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
 	result.m[2][2] = scale.z;
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
+	result.m[3][3] = 1;
 
 	return result;
 }
