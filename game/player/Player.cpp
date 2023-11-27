3373add471699d3a7521d69f5397ce16535cc7d6
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-11-28 05:23:15 +0900

    プレイヤーの生成

--- game/player/Player.cpp
+++ game/player/Player.cpp
@@ -1,1 +1,24 @@
 #include "Player.h"
+
+PlayerBall::PlayerBall() {};
+PlayerBall::~PlayerBall() {
+
+}
+
+void PlayerBall::Initialize2() {
+	directionalLight_.color = { 1.0f,1.0f,1.0f,1.0f };
+	directionalLight_.direction = { 0.0f,-1.0f,0.0f };
+	directionalLight_.intensity = 1.0f;
+};
+
+void PlayerBall::Update() {
+
+};
+
+void PlayerBall::Draw(Transform transform, uint32_t index, Transform viewProjection) {
+	player_->Draw({ 1.0f,1.0f,1.0f,1.0f }, transform, index, viewProjection, directionalLight_);
+};
+
+void PlayerBall::Release() {
+
+}
--- game/player/Player.h
+++ game/player/Player.h
@@ -1,5 +1,18 @@
 #pragma once
-class Player
+#include "Sphere.h"
+#include "MatrixCalculation.h"
+#include "../Vertex.h"
+
+class PlayerBall
 {
+public:
+	PlayerBall();
+	~PlayerBall();
+	void Initialize2();
+	void Update();
+	void Draw(Transform transfrom, uint32_t index, Transform viewProjection);
+	void Release();
+	Sphere* player_ = nullptr;
+	DirectionalLight directionalLight_;
 };
 
